a141234b622ee9fc5f2ae9d7ec8abc41f4c81c6c
Tomas Mejia <tomatitus2@gmail.com>
2025-04-17 11:46:57 -0400

    Add function for generating verilog lut

--- demo materials/lut_generator.py
+++ demo materials/lut_generator.py
@@ -3,29 +3,34 @@ import csv
 
 # SIZE CONSTANTS
 LUT_ADDRESS_SIZE = 7   # Create LUT with 2^x entries 
-LUT_BIT_DEPTH    = 10  # Each entry is x bits
+LUT_BIT_DEPTH    = 12  # Each entry is x bits
+
+def f(x):
+    return math.sin(x) + 0.5*math.sin(10*x)
 
 def main():
     x_lut = create_lut(
-        function = math.sin,
+        function = math.cos,
         domain = [0, 2*math.pi],
         periodic = True
     )
     print()
     y_lut = create_lut(
-        function = math.cos,
+        function = math.sin,
         domain = [0, 2*math.pi],
         periodic = True
     )
 
-    write_csv(x_lut, y_lut)
+    # write_csv("waveform.csv", x_lut, y_lut)
+    write_verilog_LUT("verilog_lut.txt", x_lut)
+
     # atan_lut = create_lut(
     #     function = math.atan,
     #     domain = [-0.466307658155, 0.466307658155]
     # )
 
 
-def create_lut(*, function, domain, periodic=False):
+def create_lut(*, function, domain, periodic=False, binary=False):
     num_steps = 2**LUT_ADDRESS_SIZE
     if periodic:
         step_size = (domain[1] - domain[0])/(num_steps)
@@ -51,14 +56,15 @@ def create_lut(*, function, domain, periodic=False):
         lut[i] = round((lut[i] - minimum) / (maximum - minimum) * (2**LUT_BIT_DEPTH - 1))
         
         # Convert to string of 1s and 0s
-        lut[i] = bin(round(lut[i]))[2:].zfill(LUT_BIT_DEPTH)
+        if binary:
+            lut[i] = bin(round(lut[i]))[2:].zfill(LUT_BIT_DEPTH)
 
         print(str(i).zfill(2) + ": ", lut[i])
     
     return lut
 
 
-def write_csv(x_lut, y_lut):
+def write_csv(filename, x_lut, y_lut):
     # NOTE: all data signals are doubled in length since clock edges are in between.
     DAC_A_COMMAND = "0001"
     DAC_B_COMMAND = "1010"
@@ -81,11 +87,19 @@ def write_csv(x_lut, y_lut):
 
     # Write to CSV
     rows = zip(data, clk, cs)
-    with open('waveform.csv', mode='w', newline='', encoding='utf-8') as file:
+    with open(filename, mode="w", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(['Pin0', 'Pin1', 'Pin2'])
+        writer.writerow(["Pin0", "Pin1", "Pin2"])
         writer.writerows(rows)
 
+
+def write_verilog_LUT(filename, lut):
+    with open(filename, mode="w", encoding="utf-8") as file:
+        for index in range(0, len(lut)):
+            value = lut[index]
+            line = f"rom[{index}] = 12'd{value};\n"
+            file.write(line)
+        
 
 
 # ========== HELPER FUNCTIONS ==========
